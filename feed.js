f19b9054b49e9760330dd913bc963d06eaf2b61c
tetsurou21 <tetsurou21@gmail.com>
2013-10-26 17:57:26 +0900

    csvにfeed情報を保存するようにした

--- feed.js
+++ feed.js
@@ -1,28 +1,35 @@
-var FeedParser = require('feedparser'),
-request = require('request'),
-WebSocket = require('ws');
+var FeedParser = require('feedparser')
+  , request = require('request')
+  , WebSocket = require('ws')
+  , csv = require('csv')
+  , fs = require('fs');
 
 var ws = new WebSocket('ws://localhost:3000');
-var feedTitle;
 
-request('http://feeds.reuters.com/reuters/topNews')
-  .pipe(new FeedParser())
-  .on('meta', function(meta) {
-    feedTitle = meta.title;
-  })
-  .on('readable', function () {
-    var stream = this, item;
-    while (item = stream.read()) {
-      var jstr = JSON.stringify({
-	type: 'feed',
-	user: feedTitle,
-	message: item.title
+csv()
+  .from.stream(fs.createReadStream('feeds.csv'))
+  .on('record', function(row, index) {
+    var id = row[0];
+    var url = row[1];
+    var title = row[2];
+    request(url)
+      .pipe(new FeedParser())
+      .on('meta', function(meta) {
+	feedTitle = meta.title;
+      })
+      .on('readable', function () {
+	var stream = this, item;
+	while (item = stream.read()) {
+	  var jstr = JSON.stringify({
+	    type: 'feed',
+	    user: title,
+	    message: item.title
+	  });
+	  
+	  ws.send(jstr);
+	}
+      })
+      .on('end', function() {
+	ws.terminate();
       });
-
-      ws.send(jstr);
-    }
-  })
-  .on('end', function() {
-    ws.terminate();
   });
-

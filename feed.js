73b2b85939acd83481ec0e3efc105a3d2549e2f2
tetsurou21 <tetsurou21@gmail.com>
2013-10-26 10:20:26 +0900

    remove websocket code on feed.js

--- feed.js
+++ feed.js
@@ -1,8 +1,6 @@
 var FeedParser = require('feedparser')
 , request = require('request');
 
-var ws = new WebSocket("ws://localhost:3000/");
-
 request('http://b.hatena.ne.jp/hotentry.rss')
   .pipe(new FeedParser())
   .on('readable', function () {
@@ -15,18 +13,13 @@ request('http://b.hatena.ne.jp/hotentry.rss')
 
       console.log('jstr: ' + jstr);
 
-      ws.send(JSON.stringify({
-	'user': 'anonymous',
-	'message': message.val()
-      }));
-
-      // request({
-      // 	'uri': 'http://localhost:3000/posts',
-      // 	'method': 'POST',
-      // 	'body': jstr,
-      // 	'headers': {
-      // 	  'Content-Type': 'application/json'
-      // 	}
-      // });
+      request({
+      	'uri': 'http://localhost:3000/posts',
+      	'method': 'POST',
+      	'body': jstr,
+      	'headers': {
+      	  'Content-Type': 'application/json'
+      	}
+      });
     }
   })

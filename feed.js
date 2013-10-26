0af7255c80b6ee8beed02a9d73a19f8eeddde68f
tetsurou21 <tetsurou21@gmail.com>
2013-10-26 09:05:58 +0900

    change indent width to 2

--- feed.js
+++ feed.js
@@ -1,25 +1,23 @@
 var FeedParser = require('feedparser')
 , request = require('request');
 
-
-
 request('http://b.hatena.ne.jp/hotentry.rss')
   .pipe(new FeedParser())
   .on('readable', function () {
-      var stream = this, item;
-      while (item = stream.read()) {
-	  var jstr = JSON.stringify({
-	      user: 'feed',
-	      message: item.title
-	  });
-	  console.log('jstr: ' + jstr);
-	  request({
-	      'uri': 'http://localhost:3000/posts',
-	      'method': 'POST',
-	      'body': jstr,
-	      'headers': {
-		'Content-Type': 'application/json'
-	      }
-	  });
-      }
+    var stream = this, item;
+    while (item = stream.read()) {
+      var jstr = JSON.stringify({
+	user: 'feed',
+	message: item.title
+      });
+      console.log('jstr: ' + jstr);
+      request({
+	'uri': 'http://localhost:3000/posts',
+	'method': 'POST',
+	'body': jstr,
+	'headers': {
+	  'Content-Type': 'application/json'
+	}
+      });
+    }
   })
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,12 @@
 var spawn = require('child_process').spawn;
 
 exports.create = function(req, res){
-    var message = req.body;
-    console.log("adding message: " + JSON.stringify(message));
-    console.log('header: ' + JSON.stringify(req.headers));  
-    var p = spawn('echo', [message.message]);
-    p.stdout.on('data', function (data) {
-	console.log('echo: ' + data);
-    });
-    res.send(message);
+  var message = req.body;
+  console.log("adding message: " + JSON.stringify(message));
+  console.log('header: ' + JSON.stringify(req.headers));  
+  var p = spawn('echo', [message.message]);
+  p.stdout.on('data', function (data) {
+    console.log('echo: ' + data);
+  });
+  res.send(message);
 };

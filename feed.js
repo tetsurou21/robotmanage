76c8ef185f740bf270e01946d41c6c2dadcfe68c
tetsurou21 <tetsurou21@gmail.com>
2013-10-26 17:09:44 +0900

    feedのタイトルをユーザにした

--- feed.js
+++ feed.js
@@ -3,15 +3,19 @@ request = require('request'),
 WebSocket = require('ws');
 
 var ws = new WebSocket('ws://localhost:3000');
+var feedTitle;
 
-request('http://b.hatena.ne.jp/hotentry.rss')
+request('http://feeds.reuters.com/reuters/topNews')
   .pipe(new FeedParser())
+  .on('meta', function(meta) {
+    feedTitle = meta.title;
+  })
   .on('readable', function () {
     var stream = this, item;
     while (item = stream.read()) {
       var jstr = JSON.stringify({
 	type: 'feed',
-	user: 'フィード',
+	user: feedTitle,
 	message: item.title
       });
 
